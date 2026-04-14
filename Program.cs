1e7468dab2c052320af69434d1acdd110fc083d9
joseaguilar <ja6150929@gmail.com>
2026-04-14 16:24:47 -0600

    Integración de Menú de Inicio

--- Program.cs
+++ Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Iniciando Menú de opciones
+            int opcion = 0;
+            while (opcion != 3)
+            {
+                Console.Clear();
+                Console.WriteLine("--- UNIVERSIDAD DON BOSCO ---");
+                Console.WriteLine("Desafío 2 - Programación de Algoritmos");
+                Console.WriteLine("1. Ejercicio 1: Juego del Ahorcado");
+                Console.WriteLine("2. Ejercicio 2: Sistema de Notas");
+                Console.WriteLine("3. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1: EjercicioAhorcado(); 
+                        break;
+                        case 2: EjercicioNotas(); 
+                        break;
+                        case 3: Console.WriteLine("Saliendo..."); 
+                        break;
+                        default: Console.WriteLine("Opción no válida."); 
+                        break;
+                    }
+                }
+                if (opcion != 3) 
+                { Console.WriteLine("\nPresione cualquier tecla para continuar..."); 
+                Console.ReadKey(); 
+                }
+            }
+        }
+        //Division de ejercicios
+        static void EjercicioAhorcado()
+        {
+            Console.WriteLine("Iniciando Juego del Ahorcado...");
+        }
+
+        static void EjercicioNotas()
+        {
+            Console.WriteLine("Iniciando Sistema de Registro de Notas...");
+        }
+    }
+}
+
