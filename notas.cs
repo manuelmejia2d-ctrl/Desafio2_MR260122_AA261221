601779dad5a1c9a95e1ea86d92ed8b1db41ee5b8
joseaguilar <ja6150929@gmail.com>
2026-04-16 16:39:00 -0600

    Codificacion de proceso de entrada, calculos y validaciones

--- notas.cs
+++ notas.cs
@@ -9,14 +9,53 @@ namespace Desafio2
     {
         //ESTRUCTURA BASE
         Console.Clear();
-        Console.Write("\nBIENVENIDO AL SISTEMA DE NOTAS: ");
+        Console.Write("\n******BIENVENIDO AL SISTEMA DE NOTAS******");
         Console.Write("\n");
         Console.Write("Ingrese la cantidad de estudiantes: ");
         int n = int.Parse(Console.ReadLine());
 
         string[] nombres = new string[n];
         double[] notas = new double[n];
-        
+        // proceso de ingreso y validacion
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"\nEstudiante {i + 1}");
+
+            Console.Write("Nombre: ");
+            nombres[i] = Console.ReadLine();
+
+            double nota;
+
+            while (true)
+            {
+                Console.Write("Nota (0 - 10): ");
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out nota) && nota >= 0 && nota <= 10)
+                    break;
+
+                Console.WriteLine("Por favor, solo ingrese numeros.");
+            }
+
+            notas[i] = nota;
+        }
+        //Proceso de claculos
+        double suma = 0;
+        double max = notas[0];
+        double min = notas[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            suma += notas[i];
+
+            if (notas[i] > max)
+                max = notas[i];
+
+            if (notas[i] < min)
+                min = notas[i];
+        }
+
+        double promedio = suma / n;
     }  
    }    
    }
